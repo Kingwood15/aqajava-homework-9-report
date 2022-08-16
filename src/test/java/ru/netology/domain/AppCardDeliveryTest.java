6efeafdf7cb4432447b54caa25522a451651c438
Азат Ильдарханов <defcon797@gmail.com>
2022-08-16 11:22:52 +0300

    setup allure and integrated with Selenide

--- src/test/java/ru/netology/domain/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/domain/AppCardDeliveryTest.java
@@ -2,6 +2,10 @@ package ru.netology.domain;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -14,6 +18,16 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AppCardDeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void openBrowser() {
